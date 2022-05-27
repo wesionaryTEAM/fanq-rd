18519cc49f402fa8f71ae1c779dd32908575f91b
UjjwolKayastha <uzol123@gmail.com>
2022-05-27 12:50:17 +0545

    feat: implement profile user page display and edit

--- apps/fanq/pages/login.tsx
+++ apps/fanq/pages/login.tsx
@@ -1,12 +1,12 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import styles from '../styles/Home.module.css';
-import { supabase } from '../lib/supabaseClient';
-import { Button, Flex, Input } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import styles from "../styles/Home.module.css";
+import { supabase } from "../lib/supabaseClient";
+import { Button, Flex, Input } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 const Login: NextPage = () => {
   const [loading, setLoading] = useState(false);
@@ -26,8 +26,7 @@ const Login: NextPage = () => {
         password: data?.password,
       });
       if (error) throw error;
-
-      router.push('/profile');
+      router.push("/profile");
     } catch (error) {
       alert(error);
     } finally {
@@ -49,10 +48,10 @@ const Login: NextPage = () => {
           <a href="https://nextjs.org">LOGIN</a>
         </h1>
 
-        <Flex justify={'center'}>
+        <Flex justify={"center"}>
           <form onSubmit={handleSubmit(handleLogin)}>
             <Flex
-              flexDirection={'column'}
+              flexDirection={"column"}
               gap="4"
               marginTop={10}
               maxWidth={400}
@@ -60,14 +59,14 @@ const Login: NextPage = () => {
               {/* register your input into the hook by invoking the "register" function */}
               <Input
                 placeholder="email"
-                {...register('email', { required: true })}
+                {...register("email", { required: true })}
               />
               {errors.email && <span>Email is required</span>}
 
               <Input
-                type={'password'}
+                type={"password"}
                 placeholder="password"
-                {...register('password', { required: true })}
+                {...register("password", { required: true })}
               />
               {errors.password && <span>Password is required</span>}
 
@@ -85,7 +84,7 @@ const Login: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
--- apps/fanq/pages/profile.tsx
+++ apps/fanq/pages/profile.tsx
@@ -1,27 +1,139 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { Avatar, Box, Button, Flex, Input, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { supabase } from "../lib/supabaseClient";
 
-export default function Profile({ session }: any) {
+
+interface IEditForm {
+  email: string;
+  first_name: string;
+  last_name: string;
+  profile_image: string;
+  password?: "123456";
+}
+
+
+const Profile = () => {
+  const user = supabase.auth.user();
+  console.log("USER", user);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IEditForm>({
+    defaultValues: {
+      email: user?.email,
+      first_name: user?.user_metadata?.first_name,
+      last_name: user?.user_metadata?.last_name,
+      profile_image: user?.user_metadata?.profile_image,
+    },
+  });
+  console.log("ERROEAS", errors);
+
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
   const router = useRouter();
 
-  const handleSignOut = () => {
-    supabase.auth.signOut();
-    router.replace('/login');
+  const editUser = async (data: any) => {
+    const { profile_image, ...credentials } = data;
+
+    console.info("FROM EDIT USER", profile_image, credentials);
+
+    try {
+      const { user, error } = await supabase.auth.update({
+        email: credentials?.email,
+        data: {
+          first_name: credentials?.firstname,
+        },
+      });
+      console.info("FROM EDIT TRY CATCH", user, error);
+    } catch (error) {}
   };
 
   return (
-    <div className="form-widget">
+    <div>
       <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session?.user?.email} disabled />
-      </div>
-
-      <div>
-        <button className="button block" onClick={handleSignOut}>
+        <button
+          onClick={() => {
+            supabase.auth.signOut();
+            router.push("/");
+          }}
+        >
           Sign Out
         </button>
+        {isEditMode ? (
+          <Flex justify={"center"}>
+            <form onSubmit={handleSubmit(editUser)}>
+              <Flex
+                flexDirection={"column"}
+                gap="4"
+                marginTop={10}
+                maxWidth={400}
+              >
+                <Flex justify={"center"}>
+                  <Avatar
+                    marginRight={4}
+                    src={`${process.env.NEXT_PUBLIC_SUPABASE_BUCKET_URL}/${user?.user_metadata.profile_image}`}
+                  />
+                  <Input {...register("profile_image")} type="file" />
+                </Flex>
+                {/* register your input into the hook by invoking the "register" function */}
+                <Input
+                  placeholder="email"
+                  {...register("email", { required: true })}
+                />
+                <Input placeholder="first name" {...register("first_name")} />
+                <Input placeholder="last name" {...register("last_name")} />
+                {/* include validation with required or other standard HTML validation rules */}
+                <Input
+                  type={"password"}
+                  placeholder="password"
+                  {...register("password")}
+                />
+
+                {errors && <span>Please fill in properly.</span>}
+                <Button type="submit" isLoading={loading} colorScheme="teal">
+                  Edit
+                </Button>
+                <Button
+                  onClick={() => setIsEditMode(false)}
+                  isLoading={loading}
+                  colorScheme="teal"
+                >
+                  Cancel
+                </Button>
+              </Flex>
+            </form>
+          </Flex>
+        ) : (
+          <Flex justifyContent={"center"} width="100%">
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+              gap={5}
+            >
+              <Avatar
+                src={`${process.env.NEXT_PUBLIC_SUPABASE_BUCKET_URL}/${user?.user_metadata.profile_image}`}
+              />
+              <Flex>
+                <span>Email: </span> <Text marginLeft={2}>{user?.email}</Text>
+              </Flex>
+              <Flex>
+                <span>Username: </span>{" "}
+                <Text marginLeft={2}>{user?.user_metadata?.first_name}</Text>{" "}
+                <Text marginLeft={1}>{user?.user_metadata?.last_name}</Text>
+              </Flex>
+            </Box>
+            <Button onClick={() => setIsEditMode(true)}>Edit</Button>
+          </Flex>
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default Profile;
